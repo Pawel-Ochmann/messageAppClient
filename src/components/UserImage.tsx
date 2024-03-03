e98704b3d89c0531ba6c49ca37d06aff87526dc1
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2024-03-03 18:39:25 +0100

    Create logic for changing image of a user, sending image to the server

--- src/components/UserImage.tsx
+++ src/components/UserImage.tsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 const UserImage = ({ userName }: { userName: string }) => {
   const imagePath = getAddress(`/${userName}/avatar`);
   const [imageAddress, setImageAddress] = useState<string | null>(null);
+  const [openDialog, setOpenDialog] = useState(false);
 
   useEffect(() => {
     const fetchImage = async () => {
@@ -17,17 +18,78 @@ const UserImage = ({ userName }: { userName: string }) => {
         }
       } catch (error) {
         console.error('Error fetching image:', error);
-        setImageAddress(null); 
+        setImageAddress(null);
       }
     };
 
     fetchImage();
   }, [imagePath]);
 
+  const openImageDialog = () => {
+    setOpenDialog(!openDialog);
+  };
+
+  const ImageForm = () => {
+    const [file, setFile] = useState<File | null>(null);
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const selectedFile = event.target.files && event.target.files[0];
+      setFile(selectedFile);
+    };
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+
+      try {
+        if (file) {
+          const formData = new FormData();
+          formData.append('avatar', file);
+          console.log(file);
+          await axios.post(getAddress(`/${userName}/avatar`), formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+          });
+        } else {
+          await axios.post(getAddress(`/${userName}/avatar`), null);
+        }
+
+        setFile(null);
+      } catch (error) {
+        console.error('Error submitting form:', error);
+      }
+    };
+
+    return (
+      <dialog open={openDialog}>
+        <button onClick={openImageDialog}>X</button>
+        <h2>Image Form</h2>
+        <form onSubmit={handleSubmit} encType='multipart/form-data'>
+          <div>
+            <input type='file' name='avatar' onChange={handleFileChange} />
+          </div>
+          <div>
+            <button type='submit'>
+              {!file ? 'Remove Current Image' : 'Add Image'}
+            </button>
+          </div>
+        </form>
+      </dialog>
+    );
+  };
+
   return imageAddress ? (
-    <img src={imageAddress} alt={`${userName}'s profile`} />
+    <>
+      <button onClick={openImageDialog}>
+        <img src={imageAddress} alt={`${userName}'s profile`} />
+      </button>
+      <ImageForm />
+    </>
   ) : (
-    <button>{userName[0]}</button>
+    <>
+      <button onClick={openImageDialog}>{userName[0]}</button>
+      <ImageForm />
+    </>
   );
 };
 
